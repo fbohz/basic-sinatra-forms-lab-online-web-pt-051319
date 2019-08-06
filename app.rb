66f816d05b05a3e3e895df84f07f7229ef99cc78
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-08-06 16:47:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,19 +10,11 @@ class App < Sinatra::Base
   post '/team' do 
     @name = params[:name]
     @coach = params[:coach]
-<<<<<<< HEAD
     @pg = params[:pg]
     @sg = params[:sg]
     @pf = params[:pf]
     @sf = params[:sf]
     @c = params[:c]
-=======
-    @point_g = params[:point_g]
-    @shooting_g = params[:shooting_g]
-    @power_f = params[:power_f]
-    @small_f = params[:small_f]
-    @center = params[:center]
->>>>>>> 71714133b0284d7945086e710ca47d8dfa869b68
     erb :team
   end   
 
